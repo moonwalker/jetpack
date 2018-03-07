3b2f73e06dec237641722dad46f7aa057c7f0266
rucsi <rucsi6@gmail.com>
2018-03-06 21:09:54 -0500

    canonicalLocales

--- src/webpack/getRoutes.js
+++ src/webpack/getRoutes.js
@@ -1,24 +1,36 @@
 const async = require('async')
 const fetch = require('isomorphic-fetch')
 
-const getSitemapRoutes = (pathLocales, product) => {
+const getSitemapRoutes = ({ pathLocales, canonicalLocales }, product) => {
   const localeRegexp = new RegExp('^/([a-z]{2}(-[a-z]{2})?)(.*)');
   return function(sitemap, cb) {
     async.map(sitemap.localeRoutes, (r, rCb) => {
       const localeMatch = r.match(localeRegexp);
       const locale = localeMatch[1];
       const path = `/${localeMatch[3]}/`.replace(/\/\//g, '\/');
-      const locales = pathLocales[path].filter(l => (l !== locale));
-      rCb(null, { path: r, market: sitemap.marketObj, pathLocales: locales, domain: sitemap.domain, title: sitemap.title, description: sitemap.description, apiKeys: JSON.parse(product.apiKeys) });
+      const locales = pathLocales[path].filter(l => (l !== locale && !canonicalLocales[l]));
+      const canonicalLocale = canonicalLocales[locale];
+      rCb(null, { path: r, market: sitemap.marketObj, pathLocales: locales, domain: sitemap.domain, title: sitemap.title, description: sitemap.description, apiKeys: JSON.parse(product.apiKeys), canonicalLocale });
     }, (_, routes) => {
       cb(null, routes);
+    })
+  }
+}
+const reduceAsync = (items, key, value) => {
+  return (cb) => {
+    const result = {};
+    async.forEach(items, (i, aCb) => {
+      result[i[key]] = i[value];
+      aCb();
+    }, () => {
+      cb(null, result);
     })
   }
 }
 
 module.exports = (apiUrl, product) => {
   const payload = {
-    query: `{ sitemap(product: "${product}", includeLocale: true, includeNotfound: true, includeExcluded: true) { sitemaps { marketObj { code, defaultLanguage } localeRoutes } product { apiKeys } routeLocales { route, locales } } }`
+    query: `{ sitemap(product: "${product}", includeLocale: true, includeNotfound: true, includeExcluded: true) { sitemaps { marketObj { code, defaultLanguage } localeRoutes } product { apiKeys } routeLocales { route, locales } canonicalLocales { locale, canonicalLocale } } }`
   }
 
   const params = {
@@ -48,12 +60,11 @@ module.exports = (apiUrl, product) => {
     })
     .then(sitemap => {
       return new Promise(resolve => {
-        const pathLocales = {};
-        async.forEach(sitemap.routeLocales, (r, rCb) => {
-          pathLocales[r.route] = r.locales;
-          rCb();
-        }, () => {
-          async.concat(sitemap.sitemaps, getSitemapRoutes(pathLocales, sitemap.product), (_, routes) => {
+        async.parallel({
+          pathLocales: reduceAsync(sitemap.routeLocales, 'route', 'locales'),
+          canonicalLocales: reduceAsync(sitemap.canonicalLocales, 'locale', 'canonicalLocale')
+        }, (_, res) => {
+          async.concat(sitemap.sitemaps, getSitemapRoutes(res, sitemap.product), (_, routes) => {
             resolve(routes);
           })
         })
--- src/webpack/webpack.config.prd.js
+++ src/webpack/webpack.config.prd.js
@@ -20,102 +20,104 @@ const env = {
 }
 
 const clientConfig = (routes) => webpackMerge({
-  bail: true,
-  context: context,
-  devtool: 'source-map',
-  entry: {
-    main: paths.entry.main,
-    webfonts: paths.entry.webfonts,
-    segment: paths.entry.analytics
-  },
-  output: {
-    path: paths.output.path,
-    filename: paths.output.filename,
-    chunkFilename: paths.output.chunkFilename,
-    publicPath: paths.output.publicPath
-  },
-  resolve: {
-    extensions: ['.js', '.json', '.css', '.less', '.styl']
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      include: paths.src,
-      loader: 'babel-loader'
+    bail: true,
+    context: context,
+    devtool: 'source-map',
+    entry: {
+      main: paths.entry.main,
+      webfonts: paths.entry.webfonts,
+      segment: paths.entry.analytics
     },
-      {
-        test: /\.(css|less|styl)$/,
-        include: paths.src,
-        use: ExtractCssChunks.extract({
-          use: [{
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 2
-            }
-          },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: () => {
-                  autoprefixer({
-                    browsers: ['last 2 versions']
-                  });
+    output: {
+      path: paths.output.path,
+      filename: paths.output.filename,
+      chunkFilename: paths.output.chunkFilename,
+      publicPath: paths.output.publicPath
+    },
+    resolve: {
+      extensions: ['.js', '.json', '.css', '.less', '.styl']
+    },
+    module: {
+      rules: [{
+          test: /\.js$/,
+          include: paths.src,
+          loader: 'babel-loader?cacheDirectory'
+        },
+        {
+          test: /\.(css|less|styl)$/,
+          include: paths.src,
+          use: ExtractCssChunks.extract({
+            use: [{
+                loader: 'css-loader',
+                options: {
+                  modules: true,
+                  importLoaders: 2
                 }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  plugins: () => {
+                    autoprefixer({
+                      browsers: ['last 2 versions']
+                    });
+                  }
+                }
+              },
+              {
+                loader: 'less-loader'
+              },
+              {
+                loader: 'stylus-loader'
               }
-            },
-            {
-              loader: 'less-loader'
-            },
-            {
-              loader: 'stylus-loader'
-            }
-          ]
-        })
-      }
+            ]
+          })
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(paths.output.path, {
+        root: paths.root
+      }),
+      new webpack.EnvironmentPlugin(env),
+      new webpack.BannerPlugin(banner),
+      new webpack.NamedModulesPlugin(),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: ({
+          resource
+        }) => {
+          return resource && /node_modules/.test(resource);
+        }
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'manifest',
+        minChunks: ({
+          resource
+        }) => {
+          return resource && /webpack/.test(resource);
+        }
+      }),
+      new UglifyJsPlugin({
+        sourceMap: true,
+        cache: true,
+        parallel: true
+      }),
+      new ExtractCssChunks({
+        filename: paths.output.cssFilename
+      }),
+      new RenderWebpackPlugin({
+        routes: routes,
+        render: () => require(paths.render.file),
+        minimize: minimize.enabled ? minimize.minifyOptions : false
+      }),
+      new CopyWebpackPlugin([{
+        from: paths.public.root,
+        ignore: ['index.html']
+      }])
     ]
   },
-  plugins: [
-    new CleanWebpackPlugin(paths.output.path, {
-      root: paths.root
-    }),
-    new webpack.EnvironmentPlugin(env),
-    new webpack.BannerPlugin(banner),
-    new webpack.NamedModulesPlugin(),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: ({
-        resource
-      }) => {
-        return resource && /node_modules/.test(resource);
-      }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      minChunks: ({
-        resource
-      }) => {
-        return resource && /webpack/.test(resource);
-      }
-    }),
-    new UglifyJsPlugin({
-      sourceMap: true
-    }),
-    new ExtractCssChunks({
-      filename: paths.output.cssFilename
-    }),
-    new RenderWebpackPlugin({
-      routes: routes,
-      render: () => require(paths.render.file),
-      minimize: minimize.enabled ? minimize.minifyOptions : false
-    }),
-    new CopyWebpackPlugin([{
-      from: paths.public.root,
-      ignore: ['index.html']
-    }])
-  ]
-},
   createFileConfig({ context: paths.src }, env),
   createServiceWorkerConfig({
     globDirectory: paths.output.path,
@@ -124,52 +126,52 @@ const clientConfig = (routes) => webpackMerge({
 )
 
 const renderConfig = webpackMerge({
-  bail: true,
-  context: context,
-  target: 'node',
-  entry: {
-    render: paths.entry.render,
-  },
-  output: {
-    path: paths.render.path,
-    filename: paths.render.filename,
-    libraryTarget: 'commonjs2'
-  },
-  resolve: {
-    extensions: ['.js', '.json', '.css', '.less', '.styl']
-  },
-  module: {
-    rules: [{
-        test: /\.js$/,
-        include: paths.src,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(css|less)$/,
-        include: paths.src,
-        use: [{
-            loader: 'css-loader/locals',
-            options: {
-              modules: true
+    bail: true,
+    context: context,
+    target: 'node',
+    entry: {
+      render: paths.entry.render,
+    },
+    output: {
+      path: paths.render.path,
+      filename: paths.render.filename,
+      libraryTarget: 'commonjs2'
+    },
+    resolve: {
+      extensions: ['.js', '.json', '.css', '.less', '.styl']
+    },
+    module: {
+      rules: [{
+          test: /\.js$/,
+          include: paths.src,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.(css|less)$/,
+          include: paths.src,
+          use: [{
+              loader: 'css-loader/locals',
+              options: {
+                modules: true
+              }
+            },
+            {
+              loader: 'less-loader'
             }
-          },
-          {
-            loader: 'less-loader'
-          }
-        ]
-      }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(paths.render.path, {
+        root: paths.root
+      }),
+      new webpack.EnvironmentPlugin(env),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1
+      })
     ]
   },
-  plugins: [
-    new CleanWebpackPlugin(paths.render.path, {
-      root: paths.root
-    }),
-    new webpack.EnvironmentPlugin(env),
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-},
-createFileConfig({ context: paths.src, emitFile: false }, env));
+  createFileConfig({ context: paths.src, emitFile: false }, env));
 
 module.exports = { renderConfig, clientConfig }
