ee8df94610676d170fe976ba9f42e449f4cfe065
Vio <vio@beanon.com>
2019-06-05 12:29:50 +0200

    fix: Use latest CleanWebpackConfig import

--- src/webpack/webpack.config.prd.js
+++ src/webpack/webpack.config.prd.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const AssetsPlugin = require('assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
