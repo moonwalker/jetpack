aadc8270c10e7802eb6f8a522004719c89995881
Vio <vio@beanon.com>
2019-06-13 11:25:59 +0200

    Stats - use bundle-stats

--- src/webpack/config/stats.js
+++ src/webpack/config/stats.js
@@ -1,8 +1,25 @@
 const path = require('path');
 const StatsPlugin = require('stats-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { BundleStatsWebpackPlugin } = require('bundle-stats');
 
 const { paths } = require('../defaults');
+
+const ARTIFACTS_REL_DIR = path.relative(
+  paths.output.path,
+  paths.artifacts.path
+);
+
+const STATS = {
+  context: paths.src,
+  assets: true,
+  modules: true,
+  chunks: true,
+  timings: true,
+  performance: false,
+  children: false,
+  source: false,
+};
 
 module.exports = () => ({
   stats: {
@@ -14,27 +31,17 @@ module.exports = () => ({
       openAnalyzer: false,
       reportFilename: path.join(paths.artifacts.path, 'bundle-analysis.html'),
       generateStatsFile: false,
-      statsOptions: {
-        context: process.cwd(),
-        source: false
-      }
+      statsOptions: STATS
     }),
 
     new StatsPlugin(
-      path.relative(
-        paths.output.path,
-        path.join(paths.artifacts.path, 'webpack-stats.json')
-      ),
-      {
-        context: process.cwd(),
-        assets: true,
-        timings: true,
-        modules: true,
-        chunks: true,
-        performance: false,
-        children: false,
-        source: false,
-      }
-    )
+      path.join(ARTIFACTS_REL_DIR, 'webpack-stats.json'),
+      STATS
+    ),
+
+    new BundleStatsWebpackPlugin({
+      outDir: ARTIFACTS_REL_DIR,
+      stats: STATS
+    })
   ]
 });
