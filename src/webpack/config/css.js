569f217bb0100741f9d954b272561fb20c0daeb6
Vio <vio@beanon.com>
2020-03-02 10:53:00 +0100

    fix: CSS - split transform rules
    easier to override

--- src/webpack/config/css.js
+++ src/webpack/config/css.js
@@ -12,49 +12,53 @@ module.exports = (options, env) => {
   const isDevelopment = env.NODE_ENV === 'development';
   const test = /\.(css|styl)$/;
 
-  const transformRule = {
-    test,
-    include,
-    use: [
-      {
-        loader: 'css-loader',
-        options: {
-          sourceMap: true,
-          onlyLocals: node,
-          modules: {
-            context: paths.src,
-            localIdentName: isDevelopment ? '[path][name]__[local]' : '[hash:base64:5]'
-          }
-        }
-      },
-      {
-        loader: 'postcss-loader',
-        options: {
-          sourceMap: true,
-          plugins: [
-            ...(lint ? [stylelint()] : []),
-            autoprefixer(),
-            ...(isDevelopment
-              ? []
-              : [
-                  cssnano({
-                    // Avoid removing the relative (`./`) notation, webpack needs it
-                    preset: [
-                      'default',
-                      {
-                        normalizeUrl: false
-                      }
-                    ]
-                  })
-                ]),
-            postcssReporter({
-              clearAllMessages: true
-            })
-          ]
+  const transformRules = [
+    {
+      test,
+      include,
+      loader: 'css-loader',
+      options: {
+        sourceMap: true,
+        onlyLocals: node,
+        modules: {
+          context: paths.src,
+          localIdentName: isDevelopment ? '[path][name]__[local]' : '[hash:base64:5]'
         }
       }
-    ]
-  };
+    },
+    {
+      test,
+      include,
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: true,
+        plugins: [
+          ...(lint ? [stylelint()] : []),
+
+          autoprefixer(),
+
+          // Minimize when not in dev mode
+          ...(isDevelopment
+            ? []
+            : [
+                cssnano({
+                  // Avoid removing the relative (`./`) notation, webpack needs it
+                  preset: [
+                    'default',
+                    {
+                      normalizeUrl: false
+                    }
+                  ]
+                })
+              ]),
+
+          postcssReporter({
+            clearAllMessages: true
+          })
+        ]
+      }
+    }
+  ];
 
   const deliveryExtractRule = {
     test,
@@ -66,14 +70,14 @@ module.exports = (options, env) => {
   if (node) {
     return {
       module: {
-        rules: [transformRule]
+        rules: transformRules
       }
     };
   }
 
   return {
     module: {
-      rules: [deliveryExtractRule, transformRule]
+      rules: [deliveryExtractRule, ...transformRules]
     },
     plugins: [new MiniCssExtractPlugin({ filename })]
   };
