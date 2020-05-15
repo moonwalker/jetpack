411347a41b92d8a5e13b42bc36e0283349bb044c
speier <kalman.speier@gmail.com>
2020-05-15 09:50:13 +0200

    catch uncaughtException and unhandledRejection

--- src/serve/index.js
+++ src/serve/index.js
@@ -1,3 +1,12 @@
 const { serve } = require('./server');
 
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'unhandled rejection at promise', p);
+  })
+  .on('uncaughtException', (err) => {
+    console.error(err, 'uncaught exception thrown');
+    process.exit(1);
+  });
+
 serve();
