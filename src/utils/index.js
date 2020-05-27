367df53d6925476147460c8d1f3a7299f9ef3575
Vio <vio@beanon.com>
2020-05-27 09:57:24 +0200

    fix: Use same release for client/server
    Sentry can handle sam release for multiple projects as long as sourcemaps are not conflicting

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 
-const { ENV, NAMESPACE, CLIENT_RELEASE, API_HOST, TRACK_HOST } = require('../constants');
+const { ENV, NAMESPACE, RELEASE, API_HOST, TRACK_HOST } = require('../constants');
 const debug = require('./debug');
 const perf = require('./perf');
 
@@ -9,7 +9,7 @@ const getCommitId = () =>
   process.env.COMMIT || execSync('git rev-parse --short HEAD').toString().trim();
 
 const getEnvMiddleware = () => (_, reply) => {
-  const config = { ENV, NAMESPACE, RELEASE: CLIENT_RELEASE, API_HOST, TRACK_HOST };
+  const config = { ENV, NAMESPACE, RELEASE, API_HOST, TRACK_HOST };
 
   reply
     .header('Cache-Control', 'no-store, no-cache, must-revalidate')
