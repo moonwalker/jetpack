52b8ff5908b104b226387828a94da81f0fe4fb45
Vio <vio@beanon.com>
2020-10-23 15:48:02 +0200

    fix: Add process.env.PRODUCT_NAME

--- src/webpack/config/define.js
+++ src/webpack/config/define.js
@@ -3,7 +3,8 @@ const webpack = require('webpack');
 module.exports = () => ({
   plugins: [
     new webpack.DefinePlugin({
-      __SENTRY_CLIENT_DSN__: JSON.stringify(process.env.SENTRY_CLIENT_DSN)
+      __SENTRY_CLIENT_DSN__: JSON.stringify(process.env.SENTRY_CLIENT_DSN),
+      'process.env.PRODUCT_NAME': JSON.stringify(process.env.PRODUCT_NAME)
     })
   ]
 });
