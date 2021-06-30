98fc9874092f3bcc0e0b1061e43edc4ac06ed750
Vio <vio@beanon.com>
2021-06-30 12:48:34 +0200

    FIXUP: externals

--- src/webpack/webpack.config.prd.js
+++ src/webpack/webpack.config.prd.js
@@ -3,7 +3,6 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const AssetsPlugin = require('assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const nodeExternals = require('webpack-node-externals');
 
 const constants = require('../constants');
 const mergeConfigs = require('./mergeConfigs');
@@ -40,13 +39,18 @@ const SERVER_ENV = {
 
 const { context, paths } = settings;
 
+const productionConfig = {
+  bail: true,
+  mode: 'production',
+  context,
+  devtool: 'source-map',
+  plugins: [new CleanWebpackPlugin()]
+};
+
 const clientConfig = mergeConfigs(
   [
+    productionConfig,
     {
-      bail: true,
-      mode: 'production',
-      context,
-      devtool: 'source-map',
       entry: {
         init: paths.entry.init,
         main: paths.entry.main
@@ -59,13 +63,10 @@ const clientConfig = mergeConfigs(
         hashDigestLength: 8
       },
       plugins: [
-        new CleanWebpackPlugin(),
         new webpack.DefinePlugin({
           __CLIENT__: JSON.stringify(true),
           __SERVER__: JSON.stringify(false)
         }),
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        // new webpack.HashedModuleIdsPlugin(),
         new CopyWebpackPlugin({
           patterns: [
             {
@@ -73,22 +74,14 @@ const clientConfig = mergeConfigs(
             }
           ]
         }),
-        new AssetsPlugin({
-          ...paths.assets
-        })
+        new AssetsPlugin({ ...paths.assets })
       ],
       optimization: {
-        chunkIds: 'named',
-        minimizer: [new TerserPlugin({ parallel: true })]
+        minimizer: [new TerserPlugin({ parallel: 4 })]
       }
     },
     createResolveConfig(),
-    createJavascriptConfig(
-      {
-        include: paths.src
-      },
-      CLIENT_ENV
-    ),
+    createJavascriptConfig({ include: paths.src }),
     createCssConfig(
       {
         include: paths.src,
@@ -98,19 +91,13 @@ const clientConfig = mergeConfigs(
       },
       CLIENT_ENV
     ),
-    createStylusConfig({
-      include: paths.src
-    }),
+    createStylusConfig({ include: paths.src }),
     createFileConfig({ context: paths.src }, CLIENT_ENV),
     createSvgConfig({ context: paths.src }),
     createCommonChunks(),
-    createBuildInfo({
-      output: paths.output.buildInfo
-    }),
+    createBuildInfo({ output: paths.output.buildInfo }),
     createStatsConfig(),
-    createServiceWorkerConfig({
-      swDest: paths.output.swDest
-    }),
+    createServiceWorkerConfig({ swDest: paths.output.swDest }),
     createDefineConfig({ isClient: true })
   ],
   settings,
@@ -119,12 +106,9 @@ const clientConfig = mergeConfigs(
 
 const renderConfig = mergeConfigs(
   [
+    productionConfig,
     {
-      bail: true,
-      mode: 'production',
-      context,
-      target: 'node',
-      externals: [nodeExternals()],
+      externalsPresets: { node: true },
       entry: {
         render: paths.entry.render
       },
@@ -135,7 +119,6 @@ const renderConfig = mergeConfigs(
         publicPath: paths.output.publicPath
       },
       plugins: [
-        new CleanWebpackPlugin(),
         new webpack.DefinePlugin({
           __CLIENT__: JSON.stringify(false),
           __SERVER__: JSON.stringify(true)
@@ -144,38 +127,16 @@ const renderConfig = mergeConfigs(
           maxChunks: 1
         })
       ],
-      devtool: 'source-map',
       optimization: {
         minimize: false
       }
     },
     createResolveConfig(),
-    createJavascriptConfig(
-      {
-        include: paths.src
-      },
-      SERVER_ENV
-    ),
-    createCssConfig(
-      {
-        include: paths.src,
-        node: true
-      },
-      SERVER_ENV
-    ),
-    createStylusConfig({
-      include: paths.src
-    }),
-    createFileConfig(
-      {
-        context: paths.src,
-        emitFile: false
-      },
-      SERVER_ENV
-    ),
-    createSvgConfig({
-      context: paths.src
-    }),
+    createJavascriptConfig({ include: paths.src }),
+    createCssConfig({ include: paths.src, node: true }, SERVER_ENV),
+    createStylusConfig({ include: paths.src }),
+    createFileConfig({ context: paths.src, emitFile: false }, SERVER_ENV),
+    createSvgConfig({ context: paths.src }),
     createDefineConfig({ isClient: false })
   ],
   settings,
