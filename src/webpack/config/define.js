84dd7530ddb5b148e89ee6ceca77dbd2a328bd43
Vio <vio@beanon.com>
2022-08-05 15:53:20 +0200

    fix: Define API_HOST_SSR

--- src/webpack/config/define.js
+++ src/webpack/config/define.js
@@ -6,6 +6,7 @@ module.exports = (options = {}) => {
   return {
     plugins: [
       new webpack.DefinePlugin({
+        'process.env.API_HOST_SSR': JSON.stringify(process.env.API_HOST_SSR),
         'process.browser': JSON.stringify(isClient),
         'process.env.STORYBOOK': JSON.stringify(isStorybook),
         'process.env.SENTRY_CLIENT_DSN': JSON.stringify(process.env.SENTRY_CLIENT_DSN)
